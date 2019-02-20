a9e764466213e3e89d4a57cecc9c19011c7becc0
Patrick <revmydev@gmail.com>
2019-02-20 12:29:52 -0800

    very basic Angular <<>> Node connection

---
+++ backend/app.js
@@ -0,0 +1,37 @@
+const express = require('express');
+
+const app = express(); // create express app
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PATCH, DELETE, OPTIONS'
+  );
+  next();
+});
+
+app.use('/api/posts', (req, res, next) => {
+  const posts = [
+    {
+      id: 'alksdjlaksf',
+      title: 'first server-side post',
+      content: 'this is coming from the server'
+    },
+    {
+      id: 'ytekrt85',
+      title: 'second server-side post',
+      content: 'this is also coming from the server'
+    }
+  ];
+  res.status(200).json({
+    message: 'Posts fetched successfully',
+    posts: posts
+  });
+});
+
+module.exports = app;
